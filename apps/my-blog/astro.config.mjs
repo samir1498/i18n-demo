349cc7936d0425211b23882597056d0e58c462ac
Samir <bettahar.samir@outlook.com>
2024-03-03 08:06:53 +0100

    feat ✨ (i18n-demo): configuring astro i18n aut integration

--- apps/my-blog/astro.config.mjs
+++ apps/my-blog/astro.config.mjs
@@ -1,6 +1,31 @@
 import { defineConfig } from 'astro/config';
+import { i18n, filterSitemapByDefaultLocale } from 'astro-i18n-aut/integration';
+import sitemap from '@astrojs/sitemap';
+
+const defaultLocale = 'en';
+const locales = {
+  en: 'en-US', // the `defaultLocale` value must present in `locales` keys
+  fr: 'fr-CA',
+};
 
 // https://astro.build/config
 export default defineConfig({
   outDir: '../../dist/apps/my-blog',
+  trailingSlash: 'always',
+  build: {
+    format: 'directory',
+  },
+  integrations: [
+    i18n({
+      locales,
+      defaultLocale,
+    }),
+    sitemap({
+      i18n: {
+        locales,
+        defaultLocale,
+      },
+      filter: filterSitemapByDefaultLocale({ defaultLocale }),
+    }),
+  ],
 });
