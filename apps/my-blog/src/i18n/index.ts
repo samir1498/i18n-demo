51d85d8af6200ec922345949fc0c9ff3b70c24aa
Samir <bettahar.samir@outlook.com>
2024-03-03 08:06:15 +0100

    feat ✨ (i18n-demo): creating the translation objects

---
+++ apps/my-blog/src/i18n/index.ts
@@ -0,0 +1,24 @@
+export const translationsMap = {
+  fr: {
+    title: 'Bienvenue sur i18n demo.',
+    home: {
+      title: 'Accueil',
+      description: "Ceci est la page d'accueil.",
+    },
+    about: {
+      title: 'A propos',
+      description: 'Ceci est la page a propos.',
+    },
+  },
+  en: {
+    title: 'Welcome to i18n demo.',
+    home: {
+      title: 'Home',
+      description: 'This is the home page.',
+    },
+    about: {
+      title: 'About',
+      description: 'This is the about page.',
+    },
+  },
+};
